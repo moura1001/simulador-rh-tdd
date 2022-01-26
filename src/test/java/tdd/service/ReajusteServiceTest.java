7c5e2dc9b30dcaf545c952c90830e28d31b12e83
moura1001 <gmouraneto01@gmail.com>
2022-01-26 12:24:47 -0300

    commit com primeira fase de uma nova iteração do TDD para os testes unitários da funcionalidade que concede um reajuste salarial baseado no desempenho do funcionário (cenário 'A desejar')

---
+++ src/test/java/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,24 @@
+package tdd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import tdd.domain.Funcionario;
+
+public class ReajusteServiceTest {
+	
+	@Test
+	void reajusteSalarialDeveSerDe3PorcentoQuandoDesempenhoForADesejar() {
+		Funcionario funcionario = new Funcionario("Maria", LocalDate.now(), new BigDecimal("4000"));
+		ReajusteService service = new ReajusteService();
+		
+		service.concederReajuste(funcionario, Desempenho.A_DESEJAR);
+		
+		assertEquals(new BigDecimal("4120.00"), funcionario.getSalario());
+	}
+
+}
