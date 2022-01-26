bf45b77b01eb2c46b222f5a2184872d2fb28544d
moura1001 <gmouraneto01@gmail.com>
2022-01-26 11:30:04 -0300

    primeira fase do TDD para os testes unitários da funcionalidade que concede um bônus salarial ao funcionário

---
+++ src/test/java/tdd/service/BonusServiceTest.java
@@ -0,0 +1,41 @@
+package tdd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import tdd.domain.Funcionario;
+
+public class BonusServiceTest {
+	
+	@Test
+	void bonusDeveSerZeroParaFuncionarioComSalarioMuitoAlto() {
+		Funcionario funcionario = new Funcionario("João", LocalDate.now(), new BigDecimal("16000"));
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(funcionario);
+		
+		assertEquals(new BigDecimal("0.00"), bonus);
+	}
+	
+	@Test
+	void bonusDeveSer10PorCentoDoSalario() {
+		Funcionario funcionario = new Funcionario("João", LocalDate.now(), new BigDecimal("4000"));
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(funcionario);
+		
+		assertEquals(new BigDecimal("400.00"), bonus);
+	}
+	
+	@Test
+	void bonusDeveSer10PorCentoParaSalarioDeExatamente10000() {
+		Funcionario funcionario = new Funcionario("João", LocalDate.now(), new BigDecimal("10000"));
+		BonusService service = new BonusService();
+		BigDecimal bonus = service.calcularBonus(funcionario);
+		
+		assertEquals(new BigDecimal("1000.00"), bonus);
+	}
+
+}
