cf6cdcccbe335d21620b8b271acec13185abda16
moura1001 <gmouraneto01@gmail.com>
2022-01-26 18:23:17 -0300

    commit com terceira fase do TDD, refatorando o código a partir da aplicação do design pattern Strategy na funcionalidade que concede um reajuste baseado no desempenho

--- src/main/java/tdd/service/ReajusteService.java
+++ src/main/java/tdd/service/ReajusteService.java
@@ -8,18 +8,9 @@ import tdd.domain.Funcionario;
 public class ReajusteService {
 
 	public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-		if(desempenho == Desempenho.A_DESEJAR) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-			funcionario.reajustarSalario(reajuste);
-		
-		} else if(desempenho == Desempenho.BOM) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-			funcionario.reajustarSalario(reajuste);
-		
-		} else if(desempenho == Desempenho.OTIMO) {
-			BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-			funcionario.reajustarSalario(reajuste);
-		}
+		BigDecimal percentualReajuste = desempenho.percentualReajuste();
+		BigDecimal reajuste = funcionario.getSalario().multiply(percentualReajuste);
+		funcionario.reajustarSalario(reajuste);
 	}
 
 }
