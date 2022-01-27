17b0bbb4bbf05fb7b65651799943722d8996159f
moura1001 <gmouraneto01@gmail.com>
2022-01-27 11:30:43 -0300

    commit com refatoração nos testes da funcionalidade que concede um bônus salarial ao funcionário

--- src/test/java/tdd/service/BonusServiceTest.java
+++ src/test/java/tdd/service/BonusServiceTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import tdd.domain.model.Funcionario;
@@ -12,18 +13,23 @@ import tdd.domain.utils.RegraBonusException;
 
 public class BonusServiceTest {
 	
+	private BonusService service;
+	
+	@BeforeEach
+	void inicializar() {
+		service = new BonusService();
+	}
+
 	@Test
 	void funcionarioComSalarioMuitoAltoNaoDeveReceberBonus() {
 		Funcionario funcionario = new Funcionario("João", LocalDate.now(), new BigDecimal("16000"));
-		BonusService service = new BonusService();
-				
+		
 		assertThrows(RegraBonusException.class, () -> service.calcularBonus(funcionario));
 	}
 	
 	@Test
 	void bonusDeveSer10PorCentoDoSalario() {
 		Funcionario funcionario = new Funcionario("João", LocalDate.now(), new BigDecimal("4000"));
-		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(funcionario);
 		
 		assertEquals(new BigDecimal("400.00"), bonus);
@@ -32,7 +38,6 @@ public class BonusServiceTest {
 	@Test
 	void bonusDeveSer10PorCentoParaSalarioDeExatamente10000() {
 		Funcionario funcionario = new Funcionario("João", LocalDate.now(), new BigDecimal("10000"));
-		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(funcionario);
 		
 		assertEquals(new BigDecimal("1000.00"), bonus);
